ddc3448e1b2e0430292c49b73fcf21215ead636d
Gabriel <gabrielmorim98@gmail.com>
2026-04-08 12:55:56 -0300

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <div>
       <h1 className="text-red-500">Home</h1>
+      <Button>Botão</Button>
     </div>
   );
 };
